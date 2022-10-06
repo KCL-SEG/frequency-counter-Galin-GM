6859d37869ef3c3110481dd831383d978a4d27ec
Galin <galin.mihaylov@kcl.ac.uk>
2022-10-06 12:48:47 +0100

    checks frequencies

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,19 @@
 
 def frequencies(items):
     frequencies = {}
+    alreadyChecked = [];
+    repetitions = 0;
+
+    for i in items:
+
+        if not(i in alreadyChecked):
+            repetitions = items.count(i);
+            alreadyChecked.append(i);
+            frequencies[i] = repetitions;
+    
+
     # Your code goes here
     return frequencies
+
+
+frequencies(["a","c","a","d"]);
