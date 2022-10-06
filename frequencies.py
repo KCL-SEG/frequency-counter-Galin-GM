96e2b2f510e4d153da50a0e0ba456b8a7c294ccf
Galin <galin.mihaylov@kcl.ac.uk>
2022-10-06 12:58:30 +0100

    checks frequencies

--- frequencies.py
+++ frequencies.py
@@ -2,20 +2,26 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+
+    stringItems = [];
+
+    for i in items:
+        stringItems.append(str(i));
+
     frequencies = {}
     alreadyChecked = [];
     repetitions = 0;
 
-    for i in items:
+    for i in stringItems:
 
         if not(i in alreadyChecked):
-            repetitions = items.count(i);
+            repetitions = stringItems.count(i);
             alreadyChecked.append(i);
             frequencies[i] = repetitions;
-    
 
+    print(frequencies);
     # Your code goes here
     return frequencies
 
 
-frequencies(["a","c","a","d"]);
+#frequencies(["a","c","a","d", 4, 4, '4']);
